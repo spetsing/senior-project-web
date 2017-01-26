5a4da0fda796431fdf8640885427c97561222961
spetsing <spetsing@gmail.com>
2017-01-25 20:48:13 -0500

    comimit test

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -2,9 +2,9 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
 	$scope.tagline = 'To the moon and back!';
 
-
-    function onClick() {
-        alert("this worked");
+    $scope.onClick = function(){
+        alert("Shit");
     }
+
 
 });
--- public/js/services/GeekService.js
+++ public/js/services/GeekService.js
@@ -1,5 +1,5 @@
 angular.module('GeekService', []).factory('Geek', ['$http', function($http) {
-
+var x = fuckboi;
 
 
 }]);
