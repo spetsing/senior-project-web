5721abfc2dac538a8542405f54bc23903c977928
James <jfoshee2@students.kennesaw.edu>
2017-01-28 20:24:51 -0500

    Made REST call with $resource

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,5 +1,27 @@
-angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+// angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+//
+// 	$scope.tagline = 'Nothing beats a pocket protector!';
+//
+// });
 
-	$scope.tagline = 'Nothing beats a pocket protector!';
+(function (angular) {
+	'use strict';
 
-});
+	var module = angular.module('NerdCtrl', ['NerdService']);
+
+	function NerdController ($scope, NerdService) {
+
+		$scope.getDeck = function () {
+			console.log("getDeck called");
+			console.log(NerdService.get());
+		};
+
+	}
+
+	NerdController.$inject = [
+		'$scope',
+		'NerdService'
+	];
+
+	module.controller('NerdController', NerdController);
+})(window.angular );
--- public/js/services/NerdService.js
+++ public/js/services/NerdService.js
@@ -1,5 +1,21 @@
-angular.module('NerdService', []).factory('Nerd', ['$http', function($http) {
+// angular.module('NerdService', []).factory('Nerd', ['$http', function($http) {
+//
+//
+// }]);
 
+(function (angular) {
+    'use strict';
 
+    var module = angular.module('NerdService', []);
 
-}]);
+    function NerdService($resource) {
+        return $resource('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+    }
+
+    NerdService.$inject = [
+        '$resource'
+    ];
+
+    module.service('NerdService', NerdService);
+
+})(window.angular);
