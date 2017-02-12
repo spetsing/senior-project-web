1b7e44ea5966a99eaa446c7db494c773f3c64611
James <jfoshee2@students.kennesaw.edu>
2017-02-11 22:31:49 -0500

    SP-001: Added UserCtrl and Changed Radio Buttons
    Added UserCtrl for logging a user and used Angular to dynamically change
    the css styles of the radio buttons.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('sampleApp', ['ngRoute', 'ngResource', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('sampleApp', ['ngRoute', 'ngResource', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService', 'UserCtrl']);
