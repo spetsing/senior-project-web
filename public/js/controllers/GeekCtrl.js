0641094476a7ad3a2e989a3dba5be765206319fb
Eric Carboni <ericcarboni@gmail.com>
2017-03-06 18:27:55 -0500

    Fixing Payton's shit

--- public/js/controllers/GeekCtrl.js
+++ public/js/controllers/GeekCtrl.js
@@ -1,39 +1,8 @@
-// angular.module('GeekCtrl', []).controller('GeekController', function($scope) {
-//
-// 	$scope.tagline = 'The square root of life is pi!';
-//
-// });
+angular.module('GeekCtrl', []).controller('GeekController', function($scope) {
 
-(function (window, angular) {
+	$scope.tagline = 'The square root of life is pi!';
 
-	var module = angular.module('GeekCtrl', []);
-
-	function GeekController($scope, $location, $window) {
-
-		var socket = io.connect('http://localhost:8080');
-		socket.on('connect', function (data) {
-			console.log(data);
-		});
-
-		socket.on('receive-wait', function (data) {
-			if (data >= 2) {
-				$window.location.href = 'http://localhost:8080/socket-test';
-			}
-			console.log(data);
-		});
-
-		socket.emit("waiting", "I am waiting");
-
-		$scope.tagline = socket.sockets.sockets.length;
-
-	}
-
-	GeekController.$inject = [
-		'$scope',
-		'$location',
-		'$window'
-	];
-
-	module.controller("GeekController", GeekController);
-
-})(window, window.angular);
+	setTimeout(function(){
+		window.location='/socket-test';
+	}, 3000);
+});
