620cccd51d34c6d8b114dd4e4da7f549f4877c35
spetsing <spetsing@gmail.com>
2017-03-14 18:57:19 -0400

    Updating server files

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,74 @@
-module.exports = function(app) {
+module.exports = function(app,io) {
 var path = require('path');
+//battleshipDB
 
-app.get('/',function(req, res){
-  res.sendFile(path.join(__dirname+'/../public/index.html'));
-});
+	app.get('/',function(req, res){
+		res.sendFile(path.join(__dirname+'/../public/index.html'));
+	});
 
-  app.get('/socket-test',function(req, res){
-    res.sendFile(path.join(__dirname+'/../public/index.html'));
+	app.get('/socket-test',function(req, res){
+		res.sendFile(path.join(__dirname+'/../public/index.html'));
+	});
+
+	app.get('/geeks',function(req, res){
+		res.sendFile(path.join(__dirname+'/../public/index.html'));
+	});
+
+
+	app.post("/registerplayer", function(req,res) {
+		 res.setHeader('Access-Control-Allow-Origin', '*');
+		var board = req.body.board,
+			userName = req.body.userName,
+			collection = battleshipDB.collection("boards");
+
+			var returnModel = {
+				board: board,
+				userName: userName
+			}
+
+			console.log("board:  " + board  + "\nuserName:  " + userName);
+
+			collection.findOne({id: board}, function(err,documents) {
+				if(documents!==null) {
+					if(documents.assignedPlayer === "") {
+						console.log("No player is assingned to the board yet. Assigning "+ userName + " to " + board);
+						//assign player to board
+						collection.findOneAndUpdate(
+							{id: board},
+							{$set: {assignedPlayer: userName}},
+							{returnOriginal: false},
+							function(err, documents) {
+								res.status(200).json("You have been assigned to " + board);
+							});
+					} else {
+						console.log(documents);
+						res.status(400).json("A player is already assigned to " + board + ". Please make sure you are are trying to connect to the correct board");
+					}
+				} else {
+					res.status(400).json("Unable to connect to the board. Please make sure the board is powered on and try again");
+			}
+			});
   });
-};
+
+  app.post("/removeplayer", function(req,res) {
+	   res.setHeader('Access-Control-Allow-Origin', '*');
+	var board = req.body.board,
+		userName = req.body.userName,
+		collection = battleshipDB.collection("boards");
+
+
+		collection.findOneAndUpdate(
+			{id: board},
+			{$set: {assignedPlayer: ""}},
+			{returnOriginal: false},
+			function(err, documents) {
+				if(documents !== null){
+					console.log(documents);
+					res.status(200).json(userName + " has been removed from " + board);
+				} else {
+					res.status(400).json("Not able to remove user from the board at this time");
+				}
+			});
+  });
+
+  };
--- server2.js
+++ server2.js
@@ -4,30 +4,49 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var routes = require('./app/routes');
+var bodyParser = require("body-parser");
+var MongoClient = require('mongodb').MongoClient;
+var mongoCredentials = require('./mongocredentials');
+var urlTimeRecorderDB = 'mongodb://'+ mongoCredentials.dbName + ':' + mongoCredentials.password + '@127.0.0.1:27017/bs';
 
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
 
 //Points incomming request to routes files
-routes(app);
+routes(app,io);
 
 
-app.get('/', function(req, res,next) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+MongoClient.connect(urlTimeRecorderDB, function(err,database){
+	if(err) return console.log(err);
+	else {
+		console.log('Connected to MongoDB!');
+		battleshipDB = database;
+	}
 server.listen(3001, function() {
     console.log("listening on port 3001");
 });
+});
 
 var clientCount = 0;
-
 io.on('connection', function(client) {
+	console.log("player connected");
+	battleshipDB.collection("boards").findOne(
+							{assignedPlayer: ''},
+							function(err, documents) {
+								console.log(documents);
+								if(documents===null){
+									io.emit("gameReady","data");
+								}
+								//res.status(200).json("You have been assigned to " + board);
+							});
 
 
-
-    console.log('Client connected...1');
+	io.emit("join","You have established a socket conneciton to the server");
 
     client.on('join', function(data) {
         console.log(data);
@@ -49,5 +68,10 @@ io.on('connection', function(client) {
         ++clientCount;
         io.emit('receive-wait', clientCount);
     });
+
+	client.on("test", function(data) {
+
+		io.emit("gameReady","data");
+	});
 
 });
