663e04e1d1867f1abba9bf50df0740946a4f4315
spetsing <spetsing@gmail.com>
2017-03-28 11:08:34 -0400

    Moved paytons functions and variables to inside the controller

--- public/js/controllers/BoardController.js
+++ public/js/controllers/BoardController.js
@@ -1,25 +1,3 @@
-var module = angular.module('sampleApp');
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
 function BoardController($scope, Services) {
 
     $scope.services = Services;
@@ -110,6 +88,30 @@ function BoardController($scope, Services) {
         document.getElementById("msgTxt").innerHTML = "Game Over! " + data.userName + " wins!";
         modal.style.display = "block";
     }.bind($scope));
+
+    //paytons stuff
+
+    var module = angular.module('sampleApp');
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 
 }
 
