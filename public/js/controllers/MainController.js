2e0a7eae6ad166df809ed840e16e38c79dcc812c
spetsing <spetsing@gmail.com>
2017-04-23 11:36:12 -0400

    Updating master

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -6,123 +6,181 @@ function MainController($scope, $http, Services, $location) {
     $scope.services = Services;
     $scope.location = $location;
     var socket;
+    //paytons stuff
 
+
+    socket = io.connect('http://34.195.93.38:3002');
+
+        socket.on('connect', function (data) {
+            console.log(data);
+            console.log("Sockets Connected");
+
+        });
+
+    $scope.sendHit = function() {
+        var x = {
+            board: $("#board").val(),
+            cell: $("#cell").val()
+        }
+
+        socket.emit("sendHit", x);
+    }
+
+    var module = angular.module('sampleApp');
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    var fireBtn = document.getElementById("fireButton");
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
     //Hide elements as soon as page is rendered
-    $(document).ready(function() {
+    $(document).ready(function () {
         $("#waitingText").hide();
         $("#signup").hide();
     })
 
-    $scope.onClick = function(){
+    $scope.onClick = function () {
         $("#signup").show();
         $("#beginGame").hide();
     };
 
     //Called when battle button is pressed. Attempts to register user to board and then connects sockets
-   $scope.onBattle = function () {
-            debugger;
-            var board = "";
-            var userName = $("#userName")[0].value;
+    $scope.onBattle = function () {
+        debugger;
+        var board = "";
+        var userName = $("#userName")[0].value;
 
-            if ($("#radio01")[0].checked) {
-                board = "1"
-            } else {
-                board = "2"
-            }
-            console.log(board);
+        if ($("#radio01")[0].checked) {
+            board = "1"
+        } else {
+            board = "2"
+        }
+        console.log(board);
 
-            var userData = {
-                userName: userName,
-                board: board
+        var userData = {
+            userName: userName,
+            board: board
 
-            }
-            this.services.setData(userData.userName,userData.board);
+        }
+        this.services.setData(userData.userName, userData.board);
 
-            //make rest call to register player to board
+        //make rest call to register player to board
+        $.ajax({
+            type: 'POST',
+            url: 'http://34.195.93.38:3002/registerplayer',
+            data: userData,
+            dataType: 'json',
+            success: function (data) {
+                $("#signup").hide();
+                $("#waitingText").show();
+                this.connectSockets();
+
+            }.bind(this),
+            error: function (err) {
+                // this.connectSockets();
+                document.getElementById("msgTxt").innerHTML = err.responseText;
+                modal.style.display = "block";
+                //alert(err.responseText);
+            }.bind(this)
+        });
+    };
+
+
+    //connect to sockets once player has been assigned to the board
+    $scope.connectSockets = function () {
+        console.log("Connect Sockets Called");
+        var location = this.location;
+
+        socket = io.connect('http://ec2-34-195-93-38.compute-1.amazonaws.com:3002');
+
+        socket.on('connect', function (data) {
+            console.log(data);
+
+        });
+
+        socket.on("gameReady", function (data) {
             $.ajax({
-                type: 'POST',
-                url: 'http://34.195.93.38:3002/registerplayer',
-                data: userData,
-                dataType: 'json',
+                type: 'GET',
+                url: 'http://34.195.93.38:3002/resetHealth',
                 success: function (data) {
-                    $("#signup").hide();
-                    $("#waitingText").show();
-                    this.connectSockets();
+                    console.log("Game Ready Routing to board");
+                    this.location.path("/board");
+                    this.$apply();
 
                 }.bind(this),
                 error: function (err) {
-                   // this.connectSockets();
-                    alert(err.responseText);
+                    // this.connectSockets();
+                    document.getElementById("msgTxt").innerHTML = err.responseText;
+                    modal.style.display = "block";
+                    // alert(err.responseText);
                 }.bind(this)
             });
-        };
-
-
-        //connect to sockets once player has been assigned to the board
-        $scope.connectSockets = function () {
-            console.log("Connect Sockets Called");
-            var location = this.location;
-
-             socket = io.connect('http://34.195.93.38:3002');
-            socket.on('connect', function (data) {
-                console.log(data);
-
-            });
-
-            socket.on("gameReady",function(data) {
-                console.log("Game Ready Routing to board");
-                this.location.path("/board");
-                this.$apply();
-            }.bind(this));
-        }
-
+        }.bind(this));
+    }
 
 
     //removes all players from the boards. REMOVE BEFORE LIVE
-    $scope.removeUsers = function() {
+    $scope.removeUsers = function () {
 
-        var userData={
+        var userData = {
             board: "1",
-            userName:"ss"
+            userName: "ss"
         };
         $.ajax({
-                type: 'POST',
-                url: 'http://34.195.93.38:3002/removeplayer',
-                data: userData,
-                dataType: 'json',
-                success: function (data) {
-                    //this.connectSockets();
+            type: 'POST',
+            url: 'http://34.195.93.38:3002/removeplayer',
+            data: userData,
+            dataType: 'json',
+            success: function (data) {
+                //this.connectSockets();
 
 
-                }.bind(this),
-                error: function (err) {
-                    //alert(err.responseText);
-                    //this.connectSockets();
-                }.bind(this)
-            });
+            }.bind(this),
+            error: function (err) {
+                //alert(err.responseText);
+                //this.connectSockets();
+            }.bind(this)
+        });
         userData.board = "2";
         $.ajax({
-                type: 'POST',
-                url: 'http://34.195.93.38:3002/removeplayer',
-                data: userData,
-                dataType: 'json',
-                success: function (data) {
-                    //this.connectSockets();
-                }.bind(this),
-                error: function (err) {
-                    alert(err.responseText);
-                  //  this.connectSockets();
-                }.bind(this)
-            });
+            type: 'POST',
+            url: 'http://34.195.93.38:3002/removeplayer',
+            data: userData,
+            dataType: 'json',
+            success: function (data) {
+                //this.connectSockets();
+            }.bind(this),
+            error: function (err) {
+                document.getElementById("msgTxt").innerHTML = err.responseText;
+                modal.style.display = "block";
+                // alert(err.responseText);
+                //  this.connectSockets();
+            }.bind(this)
+        });
     }
 }
 
-    MainController.$inject = [
+
+MainController.$inject = [
 		'$scope',
         '$http',
         'Services',
         '$location'
 	];
 
-    module.controller('MainController', MainController);
-
+module.controller('MainController', MainController);
--- server2.js
+++ server2.js
@@ -53,6 +53,15 @@ app.get("/getShip", function(req, res, next) {
 })
 	
 
+ app.post("/hit", function(req, res) {
+
+      console.log(req.body.board + "     " + req.body.cell)
+
+      io.emit("led", {board: req.body.board , cell: req.body.cell});
+
+      res.status(200).json("Sent");
+  })
+
 
 
 //Sockets config
@@ -122,6 +131,7 @@ io.on('connection', function (client) {
             }
         })
         
+
         //Query users ships and determin if it was a hit.
         
         
@@ -130,131 +140,9 @@ io.on('connection', function (client) {
         //if not hit- send to board
     });
 
-  
-    
+ client.on("sendHit", function (data) {
+            console.log(data);
+            io.emit("led",data);
+        })
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*// app.js
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var routes = require('./app/routes');
-var bodyParser = require("body-parser");
-var MongoClient = require('mongodb').MongoClient;
-var mongoCredentials = require('./mongocredentials');
-var urlTimeRecorderDB = 'mongodb://' + mongoCredentials.dbName + ':' + mongoCredentials.password + '@127.0.0.1:27017/bs';
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(express.static(__dirname + '/public'));
-
-
-//Points incomming request to routes files
-routes(app, io);
-
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-MongoClient.connect(urlTimeRecorderDB, function (err, database) {
-    if (err) return console.log(err);
-    else {
-        console.log('Connected to MongoDB!');
-        battleshipDB = database;
-    }
-    server.listen(3001, function () {
-        console.log("listening on port 3001");
-    });
-});
-
-var clientCount = 0;
-io.on('connection', function (client) {
-    console.log("player connected");
-    battleshipDB.collection("boards").findOne({assignedPlayer: ''
-        },
-        function (err, documents) {
-            console.log(documents);
-            if (documents === null) {
-                io.emit("gameReady", "data");
-            }
-            //res.status(200).json("You have been assigned to " + board);
-        });
-
-
-    io.emit("join", "You have established a socket conneciton to the server");
-
-    client.on('join', function (data) {
-        console.log(data);
-    });
-
-    client.on("startGame", function (data) {
-        console.log(data.test);
-        console.log(data.test2);
-    });
-
-    client.on("fire", function (data) {
-        console.log(data);
-        io.emit('receive-fire', data);
-    });
-
-    client.on("waiting", function (data) {
-        console.log("A player has connected" + data);
-        console.log(io.sockets.sockets.length);
-        ++clientCount;
-        io.emit('receive-wait', clientCount);
-    });
-
-    client.on("test", function (data) {
-
-        io.emit("gameReady", "data");
-    });
-
-});*/
