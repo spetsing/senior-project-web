208c2d3fee0290fe2a3ea73d7c032b5437876dcc
spetsing <spetsing@gmail.com>
2017-04-26 16:07:12 -0400

    1

--- public/js/controllers/BoardController.js
+++ public/js/controllers/BoardController.js
@@ -132,18 +132,7 @@ function BoardController($scope, Services) {
 
     }.bind($scope));
 
-    function convertNumberToLetter(number) {
-        switch(number) {
-            case 0: return "a";break;
-            case 1: return "b";break;
-            case 2: return "c";break;
-            case 3: return "d";break;
-            case 4: return "e";break;
-            case 5: return "f";break;
-            case 6: return "g";break;
-            case 7: return "h";break;
-                     }
-    }
+
 
     $scope.resetBoard = function() {
        /* for(var x = 0; x < 8; x++)
